83712913710eca8fed8a4f8f6b71451103c3b107
expigo <kryspin.musiol@gmail.com>
2018-01-14 16:02:03 +0100

    Initial commit

---
+++ src/main/java/com/kryspinmusiol/monumentinventoryapp/MonumentInventoryAppApplication.java
@@ -0,0 +1,12 @@
+package com.kryspinmusiol.monumentinventoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonumentInventoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonumentInventoryAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kryspinmusiol/monumentinventoryapp/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.kryspinmusiol.monumentinventoryapp.controllers;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/", "/index","/login"})
+    public String getIndexPage(Model model) {
+        log.debug("Getting index page");
+
+        return "index";
+    }
+}
